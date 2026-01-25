04321e2eb80efd91a04d58d89f638f003ee34e3b
Stan <zyyu@umich.edu>
2026-01-25 15:28:12 -0500

    small changes

--- lua/dark/theme.lua
+++ lua/dark/theme.lua
@@ -12,27 +12,27 @@ function M.Hi(group, fg, bg, bold, italic)
    vim.api.nvim_set_hl(
       0, group,
       {
-	 fg     = fg,    
-	 bg     = bg,
+         fg     = fg,
+         bg     = bg,
          bold   = bold,
-	 italic = italic
+         italic = italic
       }
    )
 end
 
 local syntax_highlight = {
-   Normal           = { fg = colors.fg         , bg = colors.bg          , bold = false, italic = false }, 
+   Normal           = { fg = colors.fg         , bg = colors.bg          , bold = false, italic = false },
    Terminal         = { fg = colors.fg         , bg = colors.bg          , bold = false, italic = false },
    Visual           = { fg = colors.fg         , bg = colors.gray7       , bold = false, italic = false },
    Comment          = { fg = colors.gray8      , bg = colors.bg          , bold = true , italic = false },
    String           = { fg = colors.green      , bg = colors.bg          , bold = false, italic = false },
-   Function         = { fg = colors.purpleblue , bg = colors.bg          , bold = false, italic = false },
+   Function         = { fg = colors.dimyellow  , bg = colors.bg          , bold = false, italic = false },
    Keyword          = { fg = colors.yellow     , bg = colors.bg          , bold = false, italic = false },
    Type             = { fg = colors.blue       , bg = colors.bg          , bold = false, italic = false },
    LineNr           = { fg = colors.gray8      , bg = colors.bg          , bold = false, italic = false },
    CursorLineNr     = { fg = colors.yellow     , bg = colors.bg          , bold = false, italic = false },
    StatusLine       = { fg = colors.fg         , bg = colors.pal_h_11_L_7, bold = true , italic = false },
-   StatusLineNC	    = { fg = colors.gray1      , bg = colors.pal_h_7_L_9 , bold = false, italic = true  },
+   StatusLineNC     = { fg = colors.gray1      , bg = colors.pal_h_7_L_9 , bold = false, italic = true  },
    StatusLineTerm   = { fg = colors.fg         , bg = colors.pal_h_6_L_7 , bold = true , italic = false },
    StatusLineTermNC = { fg = colors.fg         , bg = colors.pal_h_6_L_7 , bold = false, italic = true  },
 }
